4eb8285c4e162718f3973daa2d06b934a34d9246
Paul Kosel <my@procrastination.life>
2022-06-07 23:54:44 +0200

    oof

--- static/app.pl
+++ static/app.pl
@@ -41,7 +41,7 @@ add_item_to_container(Item) :-
     %member((x-X), Item),
     %member((y-Y), Item),
     %fmts(ItemText, '~w at position ~wx, ~wy.', [ItemName, X, Y]),
-    html(ItemTxt, ItemName),
+    html(ItemTxt, Item),
     append_child(ItemElement, ItemImg),
     append_child(ItemElement, ItemTxt),
     append_child(Container, ItemElement).
